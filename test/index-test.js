aa77c5cd05e0a066c87910c205118f9bebcf95e0
Marcus Stade <marcus@stade.se>
2014-11-06 15:17:58 +0000

    Removed OAuth1 tests

--- test/index-test.js
+++ test/index-test.js
@@ -1,7 +1,7 @@
 var vows = require('vows');
 var assert = require('assert');
 var util = require('util');
-var google = require('passport-google-oauth');
+var google = require('../');
 
 
 vows.describe('passport-google-oauth').addBatch({
@@ -9,11 +9,6 @@ vows.describe('passport-google-oauth').addBatch({
   'module': {
     'should report a version': function (x) {
       assert.isString(google.version);
-    },
-    'should export OAuth strategy': function (x) {
-      assert.isFunction(google.Strategy);
-      assert.isFunction(google.OAuthStrategy);
-      assert.equal(google.Strategy, google.OAuthStrategy);
     },
     'should export OAuth 2.0 strategy': function (x) {
       assert.isFunction(google.OAuth2Strategy);
